0e33a746e45ae3f686b5bf7296fbd1624b825096
mcspe <mcspezzaferro@gmail.com>
2023-09-15 16:33:05 +0200

    [add] project

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -1,9 +1,19 @@
 package org.lessons.java.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int lng = 20;
+		Studente[] classe6 = new Studente[lng];
+		Random rnd = new Random();
+		
+		for (int i = 1; i <= lng; i++) {
+			classe6[i - 1] = new Studente(i, rnd.nextInt(101), rnd.nextFloat(1, 5));
+			System.out.println(classe6[i - 1].getInfo() + " - " + (classe6[i - 1].isPassed() ? "Promosso" : "Bocciato"));
+		}
 
 	}
 
--- src/org/lessons/java/valutazioni/Studente.java
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -1,5 +1,35 @@
 package org.lessons.java.valutazioni;
 
 public class Studente {
+	
+	public int id;
+	public int percAss;
+	public float avgVote;
+	
+	public Studente(int id, int percAss, float avgVote) {
+		this.id = id;
+		this.percAss = percAss;
+		this.avgVote = avgVote;
+	}
+	
+	public String getInfo() {
+		return "Studente: " + id + " - Media voti: " + String.format("%.2f", avgVote) + " - Percentuale assenze: " + percAss; 
+	}
+	
+	public boolean isPassed() {
+		boolean isPassed = true;
+		
+		if(percAss >= 50) {
+			isPassed = false;
+		} else {
+			if(percAss >= 25 && avgVote <= 2) {
+				isPassed = false;
+			} else if (percAss < 25 && avgVote < 2) {
+				isPassed = false;
+			}
+		}
+		
+		return isPassed;
+	}
 
 }
